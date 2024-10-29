fffb1645dd7ecbb54a42b87b65213b02d16a42d8
IamKaleb21 <hionta16@gmail.com>
2024-10-29 15:43:36 -0500

    Update favicon link and add new favicon.svg file

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 function CourseCard({ course }) {
   return (
-    <div className="border border-primary rounded-lg overflow-hidden shadow-md p-4 flex flex-col">
-      <img src={course.imageUrl} alt={course.title} className="w-full h-40 object-cover mb-4 rounded-md" />
+    <div className="border border-primary overflow-hidden shadow-md p-4 flex flex-col">
+      <img src={course.imageUrl} alt={course.title} className="w-full h-40 object-cover mb-4" />
       <h4 className="font-semibold text-primary text-md">{course.title}</h4>
       <p className="text-primary font-semibold text-lg my-2">S/. {course.price}</p>
-      <button className="mt-auto bg-primary text-white font-semibold py-2 rounded-md hover:bg-blue-800">
+      <button className="mt-auto bg-primary text-white font-semibold py-2 hover:bg-blue-800">
         Comienza Ahora
       </button>
     </div>
