402725280b9ae9878cd711d20df2c4072ba28909
IamKaleb21 <hionta16@gmail.com>
2024-10-29 15:55:37 -0500

    Update vite.config.js to configure base path and server history fallback

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import { ngrok } from 'vite-plugin-ngrok'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ngrok('2hd85YV3QxvSYZEdUF4AX2a2jtQ_4z33r4t6z2o9ck47rsnVC')], 
+  plugins: [react(), ngrok('2hd85YV3QxvSYZEdUF4AX2a2jtQ_4z33r4t6z2o9ck47rsnVC')],
+  base: '/', // Asegura que las rutas estén configuradas en la raíz
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Asegura que Vite use index.html para todas las rutas
+    },
+  },
+  server: {
+    historyApiFallback: true, // Activa la redirección en el servidor de desarrollo
+  },
 })
